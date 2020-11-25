c6a34cfad7ecf02f4313e6afa5ccd52ffdc3f050
littlett98 <37678140+littlett98@users.noreply.github.com>
2020-11-24 20:46:33 -0500

    Added Score for Obi-Wan

---
+++ SpellingBee/src/spellingbee/client/SpellingBeeClient.java
@@ -0,0 +1,36 @@
+package spellingbee.client;
+
+import javafx.application.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.paint.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+
+public class SpellingBeeClient extends Application {
+	
+	public static void main(String[] args) {
+        launch(args);
+    }
+	
+	public void start(Stage stage) {
+		
+		TabPane tabPane = new TabPane();
+		// Trevor (Obi-Wan) tab
+        Tab score = new Tab("Score", new Label("Show all score related values"));
+        tabPane.getTabs().add(score);
+        
+        VBox vBox = new VBox(tabPane);
+
+        //scene is associated with container, dimensions
+      	Scene scene = new Scene(vBox, 1000, 300); 
+      	scene.setFill(Color.BLACK);
+        
+        stage.setScene(scene);
+        stage.setTitle("Spelling Bee");
+
+        stage.show();
+	}
+}
